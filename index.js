90b1c81364d56c63d6bcc40f97df281c4ba1ea87
intrnl <intrnl28@gmail.com>
2018-07-02 18:46:51 +0700

    async is the bane of my existence

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const fs = require('fs')
-const { promisify } = require('util')
 
-const readdir = promisify(fs.readdir)
+fs.unlink('discord-diff.txt', (err) => {})
+let stream = fs.createWriteStream('discord-diff.txt', { flags: 'a' })
 
-const run = async () => {
-  fs.unlink('discord-diff.txt', (err) => {})
+fs.readdirSync('./changes').forEach(file => {
+  console.log(`Merging '${file}'`)
 
-  let stream = fs.createWriteStream('discord-diff.txt', { flags: 'a' })
-
-  const changes = await readdir('./changes/')
-  
-  changes.forEach(change => {
-    console.log(`Merging '${change}'`)
-
-    var data = fs.readFileSync(`./changes/${change}`)
-    stream.write(data + '\n')
-  })
-}
-
-run()
+  stream.write(fs.readFileSync(`./changes/${file}`) + '\n')
+})
