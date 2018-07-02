f658883555ae367203477f94601da2fec9ac6b29
intrnl <intrnl28@gmail.com>
2018-07-02 13:16:26 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const { promisify } = require('util')
+
+const readdir = promisify(fs.readdir)
+const readfile = promisify(fs.readFile)
+
+const run = async () => {
+  fs.unlink('discord-diff.txt', (err) => {
+    if (err) throw err
+  })
+
+  let stream = fs.createWriteStream('discord-diff.txt', { flags: 'a' })
+
+  const changes = await readdir('./changes/')
+  changes.forEach(async change => {
+    let data = await readfile(`./changes/${change}`)
+    
+    stream.write(data + '\n')
+  })
+}
+
+run()
